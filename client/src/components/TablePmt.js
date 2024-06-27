4757a859aa46791ef6ba117089b8fa3ae357bffa
Hoangvu <hoangvu1872k2@gmail.com>
2024-06-27 17:47:30 +0700

    a

--- client/src/components/TablePmt.js
+++ client/src/components/TablePmt.js
@@ -1,14 +1,14 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import "./TablePmt.css";
 import { Scrollbars } from "react-custom-scrollbars-2";
 
 const TablePmt = (props) => {
   // console.log(props);
   // console.log(props.data);
-  let arr;
+  const [data, setData] = useState();
 
   useEffect(() => {
-    arr = props?.data?.reverse();
+    setData(props?.data?.reverse());
   }, [props]);
 
   return (
@@ -38,7 +38,7 @@ const TablePmt = (props) => {
                 </tr>
               </thead>
               <tbody>
-                {arr?.map((e, index) => (
+                {data?.map((e, index) => (
                   <tr key={index}>
                     <td>{index}</td>
                     <td>{e.d}</td>
